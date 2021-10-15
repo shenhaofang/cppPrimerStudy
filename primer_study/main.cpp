f036340b101153c114d6bcd13ebf9c1c2b8089a8
shenhaofang <lanjingren.com>
2021-10-15 11:31:22 +0800

    Initial Commit

---
+++ primer_study/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  primer_study
+//
+//  Created by 申皓方 on 2021/10/15.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
